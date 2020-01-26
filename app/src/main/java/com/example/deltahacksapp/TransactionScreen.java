694d09b0f1b36b6ed512288ac06859efcb28af5f
YangF0917 <felix.yang0917@gmail.com>
2020-01-26 10:29:22 -0500

    Minor change to transaction screen for testing

--- app/src/main/java/com/example/deltahacksapp/TransactionScreen.java
+++ app/src/main/java/com/example/deltahacksapp/TransactionScreen.java
@@ -1,39 +1,10 @@
 package com.example.deltahacksapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
-import android.content.Intent;
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.deltahacksapp.R;
-import com.example.deltahacksapp.ui.login.LoginActivity;
-import com.example.deltahacksapp.ui.login.LoginViewModel;
-import com.example.deltahacksapp.ui.login.LoginViewModelFactory;
 
 public class TransactionScreen extends AppCompatActivity {
     ListView trans;
